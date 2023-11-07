9c96e8bf95cd082f4b850f498dbff55da1fffa2c
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-08 03:05:26 +0800

    #22 feat: show basic results page content

--- src/routes/hackathons/[hackathonId]/results/+page.server.ts
+++ src/routes/hackathons/[hackathonId]/results/+page.server.ts
@@ -1,3 +1,7 @@
-export const load = async () => {
-	return {};
+import { getHackathonById } from '$lib/hackathons.js';
+
+export const load = async ({ params }) => {
+	const hackathonId = params.hackathonId;
+	const hackathon = await getHackathonById(hackathonId);
+	return { hackathon };
 };
