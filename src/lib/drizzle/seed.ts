db32dc3c29766222615bde4a544deb19a23583a7
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-08 01:18:29 +0800

    feat: update seed function

--- src/lib/drizzle/seed.ts
+++ src/lib/drizzle/seed.ts
@@ -1,29 +1,10 @@
+import type { Hackathon, User } from '$apptypes';
 import { db } from '$lib/drizzle';
-import type { NewUser, User } from '$apptypes';
-import { hackathons, users } from './schema';
+import { hackathonTeams, hackathons, users } from './schema';
 
-const newUsers: NewUser[] = [
-	{
-		name: 'Guillermo Rauch',
-		username: 'rauchg',
-		avatar: 'https://pbs.twimg.com/profile_images/1576257734810312704/ucxb4lHy_400x400.jpg'
-	},
-	{
-		name: 'Lee Robinson',
-		username: 'lee',
-		avatar: 'https://pbs.twimg.com/profile_images/1587647097670467584/adWRdqQ6_400x400.jpg'
-	},
-	{
-		name: 'Steven Tey',
-		username: 'stey',
-		avatar: 'https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg'
-	},
-	{
-		name: 'Tim Neutkens',
-		username: 'timneutkens',
-		avatar: 'https://pbs.twimg.com/profile_images/1435507969880527872/8jXw1IHR_400x400.jpg'
-	}
-];
+import newHackathonTeams from '$lib/data/newHackathonTeams.json';
+import newHackathons from '$lib/data/newHackathons.json';
+import newUsers from '$lib/data/newUsers.json';
 
 export async function seed() {
 	// Create table with raw SQL
@@ -36,23 +17,34 @@ export async function seed() {
 
 	const hackathonsData = await db.select().from(hackathons);
 	if (hackathonsData.length === 0) {
-		const insertedHackathons = await db
-			.insert(hackathons)
-			.values([
-				{
-					name: 'Daedalus Hackathon - Season 1',
-					description: `Hackathon Voting System
-					- A squad is composed of 3-5 members. Squad composition is carefully selected by the judging committee based on each individual’s skill level.
-- Choose your Squad Lead/Captain!
-- Have fun!`,
-					status: 'NEW',
-					start_date: new Date(2023, 10, 11),
-					end_date: new Date(2023, 10, 12)
-				}
-			])
-			.returning();
+		const newHackathodsData = newHackathons.map((hackathon) => ({
+			...hackathon,
+
+			start_date: new Date(2023, 10, 11),
+			end_date: new Date(2023, 10, 12)
+		})) as Hackathon[];
+		const insertedHackathons = await db.insert(hackathons).values(newHackathodsData).returning();
 		console.log(`Seeded ${insertedHackathons.length} hackathons`);
 	}
+
+	const hackathonTeamsData = await db.select().from(hackathonTeams);
+	if (hackathonTeamsData.length === 0) {
+		const insertedHackathonTeams = await db
+			.insert(hackathonTeams)
+			.values(newHackathonTeams)
+			.returning();
+		console.log(`Seeded ${insertedHackathonTeams.length} hackathon teams`);
+	}
+
+	// // seed all usernames
+	// const hackathonParticipantsData = await db.select().from(hackathonParticipants);
+	// if (hackathonParticipantsData.length === 0) {
+	// 	const insertedHackathonParticipants = await db
+	// 		.insert(hackathonParticipants)
+	// 		.values([])
+	// 		.returning();
+	// 	console.log(`Seeded ${insertedHackathonParticipants.length} hackathon participants`);
+	// }
 
 	return {};
 }
