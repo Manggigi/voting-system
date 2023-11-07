0117fe152b5d0890d9990d239edc40f045516999
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-07 23:42:42 +0800

    #18 feat: create routes

---
+++ src/lib/routes.ts
@@ -0,0 +1,19 @@
+export const routes = {
+	home: '/',
+	login: '/login',
+	logout: '/logout',
+	hackathon: {
+		base: '/hackathons',
+		hackaton: (hackathonId: number) => `/hackathons/${hackathonId}`,
+		vote: (hackathonId: number) => `/hackathons/${hackathonId}/vote`,
+		results: (hackathonId: number) => `/hackathons/${hackathonId}/results`
+	},
+	teams: {
+		base: '/teams',
+		team: (teamId: number) => `/teams/${teamId}`
+	},
+	judges: {
+		base: '/judges',
+		judge: (judgeId: number) => `/judges/${judgeId}`
+	}
+};
