eb8b39222bf84a340717a0be95e57b9193e2d408
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-07 17:29:00 +0800

    #14 feat: add example on how to use function in page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
 <script>
+	import { createUser } from '$lib/users.js';
+
 	export let data;
 	console.log(data.users);
+
+	// guide on how to use functions from other files
+	const handleSubmit = async () => {
+		await createUser({
+			name: 'John Doe',
+			username: 'haha',
+			avatar: 'https://i.pravatar.cc/300'
+		});
+	};
 </script>
 
 <h1>
