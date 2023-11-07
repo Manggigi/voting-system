550b0d07a8933b063364a265609a7c05ff658d96
Frostzeichen <cruztjomar@gmail.com>
2023-11-07 23:43:33 +0800

    Remove "unnecessary" comments

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -3,27 +3,6 @@ import { eq } from 'drizzle-orm';
 import { db } from './drizzle';
 import { hackathonJudges, hackathons, users } from './drizzle/schema';
 
-// get list of all hackathons
-// get hackathon info by id (getHackathonById(hackathons.id))
-// get hackathon team by id
-// create new hackathon
-// create new hackathon team
-// derived table
-// 	team_id REFERENCES hacakthon_team(id)
-//	final_score total_votes *
-
-// team_id		hackathon_id		judge_score		community_score
-// 2			1
-// 3			1
-// 4			1
-
-// final team score will be computed by the server. Use getFinalTeamScore() in hackathons.ts for that.
-// 	returns object {
-//
-//	}
-// hard code weights into function at hackathons.ts
-//
-
 export const createUser = async (userData: NewUser) => {
 	db.insert(users).values(userData).execute();
 };
