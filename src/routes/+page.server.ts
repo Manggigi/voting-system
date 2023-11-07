32096b09f7b1f3aa005ef36099917bcc8ffbd766
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-08 03:06:59 +0800

    #22 feat: handle login and logout state, use cookies and local storage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,9 @@
+// import { seed } from '$lib/drizzle/seed';
 import { getHackathons } from '$lib/hackathons';
 
 export async function load() {
 	const hackathons = await getHackathons();
+	// await seed();
 
 	return {
 		hackathons
