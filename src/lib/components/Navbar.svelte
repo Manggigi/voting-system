9a5aaa4d67a250e8ea7b8a7200424911c4578fb2
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-08 03:07:40 +0800

    #22 feat: handle logic on Navbar dynamic sizes

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { routes } from '$lib/routes';
 	import { userStore } from '$lib/stores';
+	import LoginButton from './LoginButton.svelte';
 
 	let menuOpen = false;
 </script>
@@ -133,62 +134,61 @@
 							{/if}
 						</div>
 					{:else}
-						<a
-							href={routes.login}
-							class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 bg-primary-700 hover:bg-gray-700 hover:text-white"
-							role="menuitem"
-							tabindex="-1"
-							id="user-menu-item-2">Login</a
-						>
+						<LoginButton />
 					{/if}
 				</div>
 			</div>
+
 			<div class="-mr-2 flex sm:hidden">
 				<!-- Mobile menu button -->
-				<button
-					type="button"
-					class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-					aria-controls="mobile-menu"
-					aria-expanded="false"
-					on:click={() => (menuOpen = !menuOpen)}
-				>
-					<span class="absolute -inset-0.5" />
-					<span class="sr-only">Open main menu</span>
-					<!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
-					<svg
-						class="block h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
+				{#if $userStore}
+					<button
+						type="button"
+						class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+						aria-controls="mobile-menu"
+						aria-expanded="false"
+						on:click={() => (menuOpen = !menuOpen)}
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-						/>
-					</svg>
-					<!--
-            Icon when menu is open.
+						<span class="absolute -inset-0.5" />
+						<span class="sr-only">Open main menu</span>
+						<!--
+					Icon when menu is closed.
 
-            Menu open: "block", Menu closed: "hidden"
-          -->
-					<svg
-						class="hidden h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
-					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
+					Menu open: "hidden", Menu closed: "block"
+				-->
+						<svg
+							class="block h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+							/>
+						</svg>
+						<!--
+					Icon when menu is open.
+
+					Menu open: "block", Menu closed: "hidden"
+				-->
+						<svg
+							class="hidden h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
+				{:else}
+					<LoginButton />
+				{/if}
 			</div>
 		</div>
 	</div>
