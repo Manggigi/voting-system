2213a3ad915c1e55da95260d9cff9d78aae07c80
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-09 22:56:16 +0800

    #26 feat: redirect to login if want to see hackathon details

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -28,21 +28,23 @@
 							href={routes.home}
 							class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</a
 						>
-						<a
-							href={routes.hackathon.base}
-							class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-							>Hackathons</a
-						>
-						<a
-							href={routes.teams.base}
-							class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-							>Teams</a
-						>
-						<a
-							href={routes.judges.base}
-							class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-							>Judges</a
-						>
+						{#if isLoggedIn}
+							<a
+								href={routes.hackathon.base}
+								class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+								>Hackathons</a
+							>
+							<a
+								href={routes.teams.base}
+								class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+								>Teams</a
+							>
+							<a
+								href={routes.judges.base}
+								class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+								>Judges</a
+							>
+						{/if}
 					</div>
 				</div>
 			</div>
--- src/routes/hackathons/[hackathonId]/+page.server.ts
+++ src/routes/hackathons/[hackathonId]/+page.server.ts
@@ -1,6 +1,8 @@
 import { getHackathonById } from '$lib/hackathons';
+import { redirect } from '@sveltejs/kit';
 
-export const load = async ({ params }) => {
+export const load = async ({ params, locals }) => {
+	if (!locals.user) throw redirect(307, '/login/discord');
 	const { hackathonId } = params;
 	const hackathon = await getHackathonById(hackathonId);
 	return { hackathon };
