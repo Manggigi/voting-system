7593afa99e62e42cdab0e23a0894384c378aafdd
Frostzeichen <cruztjomar@gmail.com>
2023-11-09 14:34:36 +0800

    Refactor: cleanup unused variables and set proper types

--- src/lib/hackathonsFinalScore.ts
+++ src/lib/hackathonsFinalScore.ts
@@ -11,45 +11,38 @@ export const getFinalTeamScore = async (hackathonId: number) => {
     let numberOfCommunityVoters = y.length;
     let x = await getJudges(hackathonId);
 	let numberOfJudges = x.length;
-	const communityWeight = 20;
+
+    const communityWeight = 20;
 	const judgeWeight = 80;
 	const maxScorePerJudge = 5;
-	let userVoterList = [];
-	let userVotesData = [];
-	let judgeVoterList = [];
-	let judgeVotesData = [];
-	let judgeVotesDataBuffer: any;
-	let totalJudgeVotesData: any;
+	let judgeVotesData: number;
+	let judgeVotesDataBuffer = [];
+
 	let output = [];
 	for (let i = 0; i < teamData.length; i++) {
-		// For every team participating in the hackathon, i, get the weighted sum of all user votes
-		// and add this sum to the weighted sum of all judge.score to get the final score.
 		teamIdBuffer = teamData[i].hackathon_teams?.id!;
 		judgeVotesDataBuffer = await getJudgeVotesByHackathonId(hackathonId, teamIdBuffer!);
-		judgeVotesData[i] = 0;
+		judgeVotesData = 0;
 		
 		for (let j = 0; j < judgeVotesDataBuffer.length; j++) {
-			judgeVotesData[i] += judgeVotesDataBuffer[j].score;
+			judgeVotesData += judgeVotesDataBuffer[j].score;
 		}
 
 		userVotesDataBuffer = await getUserVotesByHackathonId(hackathonId, teamIdBuffer!);
 
         let weightedCommunityVotes = (userVotesDataBuffer.length / (numberOfCommunityVoters - numberOfJudges)) * communityWeight;
-        let weightedJudgeVotes = ((judgeVotesData[i]) / (numberOfJudges * maxScorePerJudge)) * judgeWeight;
+        let weightedJudgeVotes = ((judgeVotesData) / (numberOfJudges * maxScorePerJudge)) * judgeWeight;
 
 		output[i] = {
 			teamName: teamData[i].hackathon_teams?.name,
 			communityVotes: userVotesDataBuffer.length,
-			judgeVotes: judgeVotesData[i],
+			judgeVotes: judgeVotesData,
             weightedCommunityVotes: weightedCommunityVotes,
 			weightedJudgeVotes: weightedJudgeVotes,
             finalScore: weightedCommunityVotes + weightedJudgeVotes
 		}
-		
-		// I don't think garbage collection will work within this loop so this here frees up the whole array:
-		judgeVotesData = []; // Nvm I'll jsut turn this into another buffer variable
 	}
-    
+
 	teamIdBuffer = 0;
 	return {
 		output
