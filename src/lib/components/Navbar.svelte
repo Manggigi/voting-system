c6510c1f889dc32bfd34d59ab1882e773abf0745
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-09 20:57:52 +0800

    #26 feat: handle register

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -247,13 +247,14 @@
 					<div class="flex-shrink-0">
 						<img
 							class="h-10 w-10 rounded-full"
-							src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+							src={user?.avatar ||
+								'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}
 							alt=""
 						/>
 					</div>
 					<div class="ml-3">
-						<div class="text-base font-medium text-white">Gian Denorte</div>
-						<div class="text-sm font-medium text-gray-400">gian@example.com</div>
+						<div class="text-base font-medium text-white">{user?.name}</div>
+						<div class="text-sm font-medium text-gray-400">{user?.username}</div>
 					</div>
 					<button
 						type="button"
