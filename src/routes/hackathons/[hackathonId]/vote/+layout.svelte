914d5dde86e7227fff2ab60742dae77cd82ded84
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-11 07:51:21 +0800

    #31 feat: handle vote page for user role

---
+++ src/routes/hackathons/[hackathonId]/vote/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Container from '@components/Container.svelte';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+</script>
+
+<main class="min-h-screen pt-20 bg-white">
+	<Container>
+		<slot />
+	</Container>
+</main>
--- src/routes/hackathons/[hackathonId]/vote/+page.svelte
+++ src/routes/hackathons/[hackathonId]/vote/+page.svelte
@@ -7,35 +7,66 @@
 	export let data;
 	let isSubmitting = false;
 	let valueSingle: string;
+	let isJudge = false;
 </script>
 
 <h2 class="h2 mb-12">{data.hackathon?.name}</h2>
-
-<form
-	method="post"
-	use:enhance={({ formElement, formData, action, cancel, submitter }) => {
-		isSubmitting = true;
-		return async ({ result, update }) => {
-			// Your form submission logic here
-			isSubmitting = false;
-			goto(routes.thankYou);
-			// update(result);
-		};
-	}}
->
-	<!-- select from list of teams -->
-	<input type="hidden" value={data.user?.id} name="user_id" />
-	<!-- <select name="team" id="team"> -->
-	<ListBox class="space-y-4 mb-4">
-		{#each data.hackathonTeams || [] as team}
-			<ListBoxItem bind:group={valueSingle} name="team" value={team.id}>{team.name}</ListBoxItem>
-			<!-- <option value={team.id}>{team.name}</option> -->
-		{/each}
-	</ListBox>
-	<!-- </select> -->
-	<button
-		disabled={!data.user?.id || isSubmitting || !valueSingle}
-		class="btn variant-filled-primary w-full"
-		type="submit">{isSubmitting ? 'Submitting...' : 'Submit Vote'}</button
+{#if !isJudge}
+	<form
+		method="post"
+		use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+			isSubmitting = true;
+			return async ({ result, update }) => {
+				// Your form submission logic here
+				isSubmitting = false;
+				goto(routes.thankYou);
+				// update(result);
+			};
+		}}
 	>
-</form>
+		<!-- select from list of teams -->
+		<input type="hidden" value={data.user?.id} name="user_id" />
+		<ListBox
+			hover="variant-outline-secondary"
+			active="variant-glass-secondary"
+			class="space-y-2 mb-4"
+		>
+			{#each data.hackathonTeams || [] as team}
+				<ListBoxItem class="bg-slate-100 " bind:group={valueSingle} name="team" value={team.id}
+					>{team.name}</ListBoxItem
+				>
+			{/each}
+		</ListBox>
+		<button
+			disabled={!data.user?.id || isSubmitting || !valueSingle}
+			class="btn btn-lg variant-filled-secondary w-full"
+			type="submit">{isSubmitting ? 'Submitting...' : 'Submit Vote'}</button
+		>
+	</form>
+{:else}
+	<form
+		method="post"
+		use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+			isSubmitting = true;
+			return async ({ result, update }) => {
+				// Your form submission logic here
+				isSubmitting = false;
+				goto(routes.thankYou);
+				// update(result);
+			};
+		}}
+	>
+		<!-- select from list of teams -->
+		<input type="hidden" value={data.user?.id} name="user_id" />
+		<select class="select" size={data.hackathonTeams?.length} name="team" id="team">
+			{#each data.hackathonTeams || [] as team}
+				<option value={team.id}>{team.name}</option>
+			{/each}
+		</select>
+		<button
+			disabled={!data.user?.id || isSubmitting || !valueSingle}
+			class="btn btn-lg variant-filled-secondary w-full"
+			type="submit">{isSubmitting ? 'Submitting...' : 'Submit Vote'}</button
+		>
+	</form>
+{/if}
