412b76c42f4dd4146624f8cf5f6727f22c9ce41c
Frostzeichen <cruztjomar@gmail.com>
2023-11-10 17:57:04 +0800

    Create server functionalities for results page

--- src/routes/hackathons/[hackathonId]/results/+page.server.ts
+++ src/routes/hackathons/[hackathonId]/results/+page.server.ts
@@ -1,10 +1,15 @@
-import { getHackathonById } from '$lib/hackathons.js';
+import { getHackathonById, getTeamsByHackathonId } from '$lib/hackathons.js';
+import { getFinalTeamScore } from '$lib/hackathonsFinalScore.js';
+
 
 export const load = async ({ params }) => {
 	const hackathonId = params.hackathonId;
 	try {
 		const hackathon = await getHackathonById(hackathonId);
-		return { hackathon };
+		const teams = await getTeamsByHackathonId(parseInt(hackathonId));
+		const finalScores = await getFinalTeamScore(parseInt(hackathonId));
+		console.log(teams.length)
+		return { hackathon, teams, finalScores };
 	} catch (e) {
 		console.log(e);
 	}
