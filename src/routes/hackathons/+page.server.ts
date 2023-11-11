959c83095a382dfa57be21c2d5a0a19b78f172e0
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-11 21:12:51 +0800

    #62 feat: convert parent logic to call fn

--- src/routes/hackathons/+page.server.ts
+++ src/routes/hackathons/+page.server.ts
@@ -1,5 +1,7 @@
-export async function load({ parent }) {
-	const { hackathons } = await parent();
+import { getHackathons } from '$lib/hackathons';
+
+export async function load() {
+	const hackathons = await getHackathons();
 
 	return {
 		hackathons
