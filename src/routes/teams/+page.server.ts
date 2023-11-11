33e9e457e054c41079222d3ae059200942074c09
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-11 20:33:03 +0800

    #57 fix: type error, refactor

--- src/routes/teams/+page.server.ts
+++ src/routes/teams/+page.server.ts
@@ -1,10 +1,12 @@
 import { getHackathonTeams } from '$lib/hackathons';
 
-export async function load() {
+export async function load({ parent }) {
+	const { hackathons } = await parent();
 	try {
 		const hackathonTeams = await getHackathonTeams();
 
 		return {
+			hackathons,
 			hackathonTeams
 		};
 	} catch (e) {
