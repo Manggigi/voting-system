94639a2072a773684d49d29e5c302a7c12ca9879
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-11 22:58:18 +0800

    feat: update hackathons page

--- src/routes/hackathons/+page.svelte
+++ src/routes/hackathons/+page.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import HackathonList from '$lib/components/HackathonList.svelte';
+	import PreviousHackatohnWinners from '@components/PreviousHackatohnWinners.svelte';
 
 	export let data;
 </script>
 
-<div>
-	<h1 class="h3">Hackathon lists</h1>
-	<HackathonList hackathons={data.hackathons} />
+<div class="space-y-8 pb-12">
+	<PreviousHackatohnWinners />
+	<hr />
+
+	<div>
+		<h3 class="h3">Upcoming Hackathons</h3>
+		<HackathonList
+			hackathons={data.hackathons?.filter((hackathon) => hackathon.status === 'NEW')}
+		/>
+	</div>
+	<hr />
+	<div>
+		<h3 class="h3">Completed Hackathons</h3>
+		<HackathonList
+			hackathons={data.hackathons?.filter((hackathon) => hackathon.status === 'COMPLETED')}
+		/>
+	</div>
 </div>
