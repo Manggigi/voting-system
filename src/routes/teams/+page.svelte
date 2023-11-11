e06230eab2aca3a1fdef3cf2222d60196faaa25a
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-11 20:42:17 +0800

    #57 feat: improve team list page

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 	export let data;
+	let groupedTeams = {};
+
+	if (data?.hackathonTeams) {
+		groupedTeams = data.hackathonTeams.reduce((acc, team) => {
+			if (!acc[team.hackathon_id]) {
+				acc[team.hackathon_id] = [];
+			}
+			acc[team.hackathon_id].push(team);
+			return acc;
+		}, {});
+	}
 </script>
 
-<h1 class="h3">full teams lists</h1>
-<ul>
-	{#each data.hackathonTeams as team}
-		<li>{team.name}</li>
-	{/each}
-</ul>
+<h1 class="h3">Teams list</h1>
+{#each Object.keys(groupedTeams) as hackathonId}
+	<div class="card p-4 my-4 text-neutral-100">
+		<h2>Hackathon {data.hackathons?.find((hackathon) => hackathon.id === hackathonId)?.name}</h2>
+		<ol class="list text-neutral-300">
+			{#each groupedTeams[hackathonId] as team}
+				<li>{team.name}</li>
+			{/each}
+		</ol>
+	</div>
+{/each}
