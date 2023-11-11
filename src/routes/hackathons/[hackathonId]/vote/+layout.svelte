d3f28caee98853fb2bae150c85bda473d179a570
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-12 03:35:25 +0800

    #70 fix: container on vote and results page

---
+++ src/routes/hackathons/[hackathonId]/vote/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Container from '@components/Container.svelte';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+</script>
+
+<Container>
+	<slot />
+</Container>
