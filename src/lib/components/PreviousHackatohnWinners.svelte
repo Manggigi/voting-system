39ddbefd9ada58d47ab0123fe055446e0bac1f09
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-11 15:56:07 +0800

    #54 feat:  create a Previous Hackathons Winner table component

---
+++ src/lib/components/PreviousHackatohnWinners.svelte
@@ -0,0 +1,23 @@
+<div>
+	<h3 class="h3 mb-2">Previous Hackathons Winner</h3>
+	<div class="table-container text-white">
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th>Team Name</th>
+					<th>Community Votes</th>
+					<th>Judge Votes</th>
+					<th>Final Score</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Team Chibog</td>
+					<td>6</td>
+					<td>64</td>
+					<td>73.27</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
