a2b257f217f921201821822fc4bd561c6bed462a
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-11 15:56:54 +0800

    #54 feat: reorder content of home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,30 @@
 <script lang="ts">
 	import HackathonList from '$lib/components/HackathonList.svelte';
+	import PreviousHackatohnWinners from '@components/PreviousHackatohnWinners.svelte';
 
 	export let data;
 </script>
 
-<div class="space-y-8">
+<div class="space-y-8 pb-12">
 	{#if data.user}
 		<h3 class="h3">Welcome, {data.user?.name}!</h3>
 		<hr />
 	{/if}
-	<div>
-		<h3 class="h3">Upcoming Hackathons</h3>
-		<HackathonList hackathons={data.hackathons} />
-	</div>
+
+	<PreviousHackatohnWinners />
 	<hr />
 
 	<div>
-		<!-- maybe wildcard of avatar of "?" -->
-		<h3 class="h3">Current Hackathons Winners</h3>
-		<p class="">
-			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident odio debitis, illo id vel
-			ratione quia, minima eligendi tempora eveniet est a rem natus fugiat sapiente alias deleniti
-			dolor quos.
-		</p>
+		<h3 class="h3">Upcoming Hackathons</h3>
+		<HackathonList
+			hackathons={data.hackathons?.filter((hackathon) => hackathon.status === 'NEW')}
+		/>
+	</div>
+	<hr />
+	<div>
+		<h3 class="h3">Completed Hackathons</h3>
+		<HackathonList
+			hackathons={data.hackathons?.filter((hackathon) => hackathon.status === 'COMPLETED')}
+		/>
 	</div>
 </div>
