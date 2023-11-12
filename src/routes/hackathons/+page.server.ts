9c017946df378c85cd434cd4d5cea3dae44cdf24
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-11-12 17:33:48 +0800

    feat: load fast /hackathons page

--- src/routes/hackathons/+page.server.ts
+++ src/routes/hackathons/+page.server.ts
@@ -1,19 +1,16 @@
 import { getHackathons } from '$lib/hackathons';
 import { getFinalTeamScore } from '$lib/hackathonsFinalScore';
 
+export const ssr = true;
 export async function load() {
-	try {
-		const hackathons = await getHackathons();
-		const currentHackathon = hackathons.filter(
-			(h) => h.name === 'Daedalus Hackathon - Season 1'
-		)[0];
-		const finalScores = await getFinalTeamScore(currentHackathon.id);
-
-		return {
-			hackathons,
-			finalScores
-		};
-	} catch (error) {
-		console.log(error);
-	}
+	const hackathons = await getHackathons();
+	const currentHackathon = hackathons.filter((h) => h.name === 'Daedalus Hackathon - Season 1')[0];
+	return {
+		hackathon: currentHackathon,
+		streamed: {
+			finalScores: new Promise((fulfil) => {
+				fulfil(getFinalTeamScore(currentHackathon.id));
+			})
+		}
+	};
 }
--- src/routes/hackathons/+page.svelte
+++ src/routes/hackathons/+page.svelte
@@ -1,13 +1,41 @@
 <script lang="ts">
 	import HackathonList from '$lib/components/HackathonList.svelte';
 	import PreviousHackatohnWinners from '@components/PreviousHackatohnWinners.svelte';
+	import SectionPlaceholder from '@components/placeholders/SectionPlaceholder.svelte';
+	import { onMount } from 'svelte';
 
 	export let data;
 </script>
 
 <div class="space-y-8 pb-12">
-	<PreviousHackatohnWinners finalScores={data.finalScores} />
-	<hr />
+	{#await data.streamed.finalScores}
+		<div>
+			<h3 class="h3 mb-2">Previous Hackathons Winner</h3>
+			<div class="table-container text-white">
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>Team Name</th>
+							<th>Community Votes</th>
+							<th>Judge Votes</th>
+							<th>Final Score</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><div class="placeholder-circle w-5"></div></td>
+							<td><div class="placeholder-circle w-5"></div></td>
+							<td><div class="placeholder-circle w-5"></div></td>
+							<td><div class="placeholder-circle w-5"></div></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	{:then finalScores}
+		<PreviousHackatohnWinners {finalScores} />
+		<hr />
+	{/await}
 
 	<div>
 		<h3 class="h3">Upcoming Hackathons</h3>
